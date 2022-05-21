25a17f56e1ffcfc68973bc14a776eb171c4e2c26
steffno <sdidionisio@gmail.com>
2022-05-21 15:46:43 +0200

    Some testing, dummy login implemented

--- chiamami_professionista/src/app/pages/login/login.page.ts
+++ chiamami_professionista/src/app/pages/login/login.page.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthServiceService } from 'src/app/services/auth-service.service';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -10,29 +11,43 @@ import { AuthServiceService } from 'src/app/services/auth-service.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  credentials: FormGroup;
+   loginFormModel: FormGroup;
+  private loginTitle: string;
+  private loginSubTitle: string;
 
   constructor(
     private fb: FormBuilder,
     private alertConrtroller: AlertController,
     private router: Router,
     private loadingController: LoadingController,
+    private translateService: TranslateService,
     private authService: AuthServiceService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.credentials=this.fb.group({
+    //dummy login validator
+    this.loginFormModel=this.fb.group({
       email: ['sdidionisio@gmail.com', [Validators.required, Validators.email]],
       password: ['1234', [Validators.required, Validators.minLength(4)]]
     });
+    this.initTranslate();
   }
 
   async login() {
     const loading = await this.loadingController.create();
     await loading.present();
 
-    this.authService.login(this.credentials.value);
+    this.authService.login(this.loginFormModel.value);
     this.router.navigateByUrl('/home', {replaceUrl:true});
+  }
+
+  async initTranslate() {
+    this.translateService.get('LOGIN_ERROR_SUB_TITLE').subscribe((data) => {
+      this.loginSubTitle = data;
+    });
+    this.translateService.get('LOGIN_ERROR_TITLE').subscribe((data) => {
+      this.loginTitle = data;
+    });
   }
 
 }
